7d9c5248e3dcf63f46efd1d8dc9ce2bfc015a52f
James Messinger <james.messinger@gmail.com>
2019-02-10 08:45:54 -0600

    Configure "babel-loader" if the `transpile` option is set

--- src/configure-webpack.ts
+++ src/configure-webpack.ts
@@ -1,11 +1,11 @@
 import { ConfigOptions } from "karma";
 import { NormalizedOptions } from "./normalize-options";
-import { mergeConfig } from "./util";
+import { hasWebpackLoader, mergeConfig } from "./util";
 
 /**
  * Configures Webpack to bundle test files and their dependencies.
  */
-export function configureWebpack(config: ConfigOptions, { testDir }: NormalizedOptions): ConfigOptions {
+export function configureWebpack(config: ConfigOptions, { testDir, transpile }: NormalizedOptions): ConfigOptions {
   config.preprocessors = mergeConfig(config.preprocessors, {
     [`${testDir}/**/*.+(spec|test).+(js|jsx|mjs)`]: ["webpack"],
   });
@@ -18,6 +18,18 @@ export function configureWebpack(config: ConfigOptions, { testDir }: NormalizedO
   config.webpack.module = mergeConfig(config.webpack.module, {
     rules: [],
   });
+
+  if (transpile && !hasWebpackLoader(config.webpack.module.rules, "babel-loader")) {
+    config.webpack.module.rules.push({
+      test: /\.(js|jsx|mjs)$/,
+      use: {
+        loader: "babel-loader",
+        options: {
+          presets: ["@babel/preset-env"]
+        }
+      }
+    });
+  }
 
   return config;
 }
